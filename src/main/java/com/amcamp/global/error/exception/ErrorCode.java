311ad1d346b8dc9f03b9a263d78d5a17dcc20b4a
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-02-22 00:25:28 +0900

    feat: 멤버 유틸리티 구현 (#21)

--- src/main/java/com/amcamp/global/error/exception/ErrorCode.java
+++ src/main/java/com/amcamp/global/error/exception/ErrorCode.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     ID_TOKEN_VERIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "ID 토큰 검증에 실패했습니다."),
+    AUTH_NOT_FOUND(HttpStatus.UNAUTHORIZED, "사용자 인증 정보를 찾을 수 없습니다. 올바른 토큰으로 요청해주세요."),
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
