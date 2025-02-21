8d10e13e328c5db89eb1a1ef509dec9b4b2ea60a
현태 최 <ht3064@naver.com>
2025-02-21 10:59:05 +0900

    chore: kakao properties 추가

--- src/main/java/com/amcamp/infra/config/properties/PropertiesConfig.java
+++ src/main/java/com/amcamp/infra/config/properties/PropertiesConfig.java
@@ -1,11 +1,13 @@
 package com.amcamp.infra.config.properties;
 
+import com.amcamp.infra.config.oauth.KakaoProperties;
 import com.amcamp.infra.config.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties({
-        RedisProperties.class
+        RedisProperties.class,
+        KakaoProperties.class
 })
 @Configuration
 public class PropertiesConfig {
