26954243d980d80cb0f4883488cb8a8906998d66
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-02-21 15:57:57 +0900

    feat: 소셜 로그인 구현 (#19)
    * chore: open feign 의존성 추가
    
    * chore: feign config 설정
    
    * chore: application.yml에 security 설정 추가
    
    * chore: kakao properties 추가
    
    * feat: 카카오 인증 서버로부터 ID 토큰 발급받는 기능 구현
    
    * chore: jwt 의존성 추가
    
    * chore: application.yml에 jwt 설정 추가
    
    * chore: jwt properties 추가
    
    * feat: jwt 유틸리티 구현
    
    * feat: 리프레시 토큰 도메인 구현
    
    * feat: ID 토큰 검증 로직 구현
    
    * chore: env 파일 gitignore 추가
    
    * chore: h2 db 설정
    
    * feat: 카카오 소셜 로그인 구현
    
    * feat: 엑세스, 리프레시 토큰 파싱 로직 구현
    
    * refactor: jwt 유틸리티 패키지 이동
    
    * refactor: config 패키지 이동
    
    * feat: 엑세스 토큰 검증 필터 추가
    
    * refactor: 리프레시 토큰을 쿠키로 관리하도록 개선
    
    * feat: 리프레시 토큰 검증 및 토큰 재발급 로직 추가

--- src/main/java/com/amcamp/global/config/security/WebSecurityConfig.java
+++ src/main/java/com/amcamp/global/config/security/WebSecurityConfig.java
@@ -1,5 +1,9 @@
-package com.amcamp.global.common.config.security;
+package com.amcamp.global.config.security;
 
+import com.amcamp.domain.auth.application.JwtTokenService;
+import com.amcamp.global.security.JwtAuthenticationFilter;
+import com.amcamp.global.util.CookieUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,15 +11,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtTokenService jwtTokenService;
+    private final CookieUtil cookieUtil;
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -34,6 +44,9 @@ public class WebSecurityConfig {
                                 .anyRequest()
                                 .authenticated());
 
+        http.addFilterBefore(
+                jwtAuthenticationFilter(jwtTokenService, cookieUtil), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
@@ -44,9 +57,15 @@ public class WebSecurityConfig {
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
+        configuration.addExposedHeader(SET_COOKIE);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtTokenService jwtTokenService, CookieUtil cookieUtil) {
+        return new JwtAuthenticationFilter(jwtTokenService, cookieUtil);
     }
 }
--- src/main/java/com/amcamp/infra/config/properties/PropertiesConfig.java
+++ src/main/java/com/amcamp/infra/config/properties/PropertiesConfig.java
@@ -1,11 +1,15 @@
 package com.amcamp.infra.config.properties;
 
+import com.amcamp.infra.config.jwt.JwtProperties;
+import com.amcamp.infra.config.oauth.KakaoProperties;
 import com.amcamp.infra.config.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties({
-        RedisProperties.class
+        RedisProperties.class,
+        KakaoProperties.class,
+        JwtProperties.class
 })
 @Configuration
 public class PropertiesConfig {
