7c483876262621fa29581a1f37b797648506f510
현태 최 <ht3064@naver.com>
2025-02-23 18:26:30 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/amcamp/global/util/CookieUtil.java
+++ src/main/java/com/amcamp/global/util/CookieUtil.java
@@ -22,4 +22,20 @@ public class CookieUtil {
 
         return headers;
     }
+
+    public HttpHeaders deleteRefreshTokenCookie() {
+        ResponseCookie refreshTokenCookie =
+                ResponseCookie.from("refreshToken", "")
+                        .path("/")
+                        .maxAge(0)
+                        .secure(false)
+                        .sameSite(Cookie.SameSite.NONE.attributeValue())
+                        .httpOnly(true)
+                        .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+        return headers;
+    }
 }
