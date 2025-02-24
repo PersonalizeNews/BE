8d25891d680711b5cedf87a56bce14cf92d3cecd
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-02-24 20:40:58 +0900

    feat: Spotify 곡 조회 기능 구현 (#43)
    * chore: spotify 의존성 추가
    
    * chore: spotify.yml 생성
    
    * chore: spotify properties 추가
    
    * chore: spotify config 추가
    
    * feat: 장르 기반 스포티파이 음악 검색 기능 구현
    
    * refactor: 장르, 년도 기반 검색 로직 개선

--- src/main/java/com/amcamp/global/error/exception/ErrorCode.java
+++ src/main/java/com/amcamp/global/error/exception/ErrorCode.java
@@ -12,6 +12,8 @@ public enum ErrorCode {
     AUTH_NOT_FOUND(HttpStatus.UNAUTHORIZED, "사용자 인증 정보를 찾을 수 없습니다. 올바른 토큰으로 요청해주세요."),
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
+
+    SPOTIFY_EXCEPTION(HttpStatus.SERVICE_UNAVAILABLE, "스포티파이 라이브러리 사용 중 예외가 발생했습니다."),
     ;
 
     private final HttpStatus httpStatus;
