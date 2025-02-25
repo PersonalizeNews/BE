ab3ce99e00a76e8259b419b31b135fa9d776bc96
HSK021843 <9902kim@hufs.ac.kr>
2025-02-25 23:35:04 +0900

    feat: wishlist 삭제 기능 추가

--- src/main/java/com/amcamp/domain/wishlist/api/WishlistController.java
+++ src/main/java/com/amcamp/domain/wishlist/api/WishlistController.java
@@ -1,11 +1,13 @@
 package com.amcamp.domain.wishlist.api;
 
 import com.amcamp.domain.wishlist.application.WishlistService;
-import com.amcamp.domain.wishlist.dto.request.WishlistCreateRequest;
 import com.amcamp.domain.wishlist.dto.response.WishlistInfoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/wishlist")
@@ -18,5 +20,21 @@ public class WishlistController {
     public void createWishlist(@RequestPart("title") String title,
                                @RequestPart("image") MultipartFile file){
        wishlistService.createWishlist(title, file);
+    }
+
+/*    @GetMapping
+    public List<WishlistInfoResponse> findAllWishlist(){
+        return wishlistService.findAllWishlist();
+    }
+
+    @PutMapping("/{wishlistId}")
+    public List<WishlistInfoResponse> editWishlist(@PathVariable Long wishlistId){
+        return wishlistService.editWishlist(wishlistId);
+    }*/
+
+    @DeleteMapping("/{wishlistId}")
+    public ResponseEntity<Void> deleteWishlist(@PathVariable Long wishlistId){
+        wishlistService.deleteWishlist(wishlistId);
+        return ResponseEntity.ok().build();
     }
 }
